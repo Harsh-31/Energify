0da8d47303a1fd897ebcc11f00f4ee1b9bde76e2
Sharma <Harsh.H.Sharma2@shell.com>
2022-09-28 16:04:15 +0530

    Tickets

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import Contact from './Components/Pages/Contact.js';
 
 import Navbar from './Components/Inc/Navbar.js';
 import GasPipeline from './Components/Pages/GasPipleine.js';
+import Tickets from './Components/Pages/Tickets';
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Routes>
+        <Route path ="/" exact element={<Tickets/>}/>
         <Route path ="/" exact element={<Home/>}/>
         <Route path ="/Contact" element={<Contact/>}/>
         <Route path ="/About" element={<About/>}/>
         <Route path ="/GasPipeline" element={<GasPipeline/>}/>
-
+        <Route path ="/Tickets" element={<Tickets/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Components/Pages/Tickets.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Tickets = () => {
+    const [isApproved, setIsApproved] = useState(false);
+    const Tick = [
+        {
+            "Ticket Number": 1,
+            "Name": "Harsh Sharma",
+            "Email": "harsh@gmail.com",
+            "Address": "371/8 Sector 10 A Gurgaon",
+            "Message": "Need maintenance activities"
+        }
+    ];
+
+
+    return (
+        <div className="users">
+            {Tick.map((user) => (
+                <>
+                    <div className='col-md-8 ticket-card'>
+                        <div className='card shadow'>
+                            <div className='card-body'>
+                                <h6 className='text-dark'>Ticket Number {user['Ticket Number']}</h6>
+                                <div className="user ticket-details">Name: {user.Name}</div>
+                                <div className="user ticket-details">Email Id: {user.Email}</div>
+                                <div className="user ticket-details">Address: {user.Address}</div>
+                                <div className="user ticket-details">Message: {user.Message}</div>
+                            </div>
+                            <button type="button" id="btnOUs" className={`btn ${isApproved ? "btn-success" : "btn-primary"}`} onClick={e => setIsApproved(true)}>{ isApproved ? "Approved" : "Not Approved"}</button>
+                        </div>
+                    </div>
+                </>
+            ))}
+        </div>
+    );
+};
+
+export default Tickets;
